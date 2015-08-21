7ef0555f838214b7fe3c72a68faab4e2c0ba2f69
leonanluppi <leonan.luppi@gmail.com>
2015-08-21 08:19:09 -0300

    authentication, get repos

--- server/controllers/github.js
+++ server/controllers/github.js
@@ -1,8 +1,13 @@
+var github = require('../../lib/');
 
 module.exports = {
   getProjects: function(req, res) {
-    github.repos.getFromOrg({org: 'wejs'}, function(res, err) {
-    })
+    github.authenticate({type: "token", token: "1d49b24d32e9d25972d67d948c4ca68c5fea9b18"});
+
+    github.repos.getFromOrg({org: 'wejs'}, function(err, res) {
+      console.log(res);
+    });
+
     return res.ok();
   }
 }
