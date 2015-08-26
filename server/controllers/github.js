ce68e40b767842219327b926789680bd53e34b6d
leonanluppi <leonan.luppi@gmail.com>
2015-08-26 10:02:56 -0300

    change req.we.config

--- server/controllers/github.js
+++ server/controllers/github.js
@@ -2,7 +2,7 @@ var github = require('../../lib/');
 
 module.exports = {
   getProjects: function(req, res) {
-    github.authenticate(req.we.config.auth.token);
+    github.authenticate(req.we.config.github.token);
     github.repos.getFromOrg({org: 'wejs'}, function(err, resGitApi) {
       var result = resGitApi.filter(function(el) {
         return el.name.indexOf(req.params.name) >= 0;
