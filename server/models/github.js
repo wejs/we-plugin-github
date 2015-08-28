e95fb15973d9a14aed6bc942556e16e4ad4ef04c
leonanluppi <leonan.luppi@gmail.com>
2015-08-28 09:14:31 -0300

    update model github

--- server/models/github.js
+++ server/models/github.js
@@ -8,7 +8,6 @@
 module.exports = function Model(we) {
   var model = {
     definition: {
-      creatorId: { type: we.db.Sequelize.BIGINT, formFieldType: null},
       name: {type: we.db.Sequelize.STRING(1500)},
       description: {type: we.db.Sequelize.TEXT},
       expireDate: {type: we.db.Sequelize.DATE}
