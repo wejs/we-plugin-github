ee8492a27c3afafd02b9894e37a99201eff9ca60
leonanluppi <leonan.luppi@gmail.com>
2015-08-28 08:33:26 -0300

    model github

---
+++ server/models/github.js
@@ -0,0 +1,24 @@
+/**
+ * Github Model
+ *
+ * @module      :: Model
+ * @description :: github model
+ *
+ */
+module.exports = function Model(we) {
+  var model = {
+    definition: {
+      creatorId: { type: we.db.Sequelize.BIGINT, formFieldType: null},
+      name: {type: we.db.Sequelize.STRING(1500)},
+      description: {type: we.db.Sequelize.TEXT},
+      expireData: {type: we.db.Sequelize.DATE}
+    },
+    options: {
+      classMethods: {},
+      instanceMethods: {},
+      hooks: {}
+    }
+  };
+
+  return model;
+};
